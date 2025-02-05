bf01ece6a89976b0dee7c4c8d85b09709dff6699
Stellar <144689207+Lunthn@users.noreply.github.com>
2025-02-05 13:53:16 +0100

    feat: hamburger-menu start and footer has socials now

---
+++ UC1-Profielpagina/Showcase-Profielpagina/wwwroot/js/hamburger-menu.js
@@ -0,0 +1,11 @@
+﻿const hamburgerIcon = document.getElementById('hamburger-icon');
+
+function toggleMenu() {
+    if (hamburgerIcon.classList.contains("bx-menu-alt-right")) {
+        hamburgerIcon.classList.remove("bx-menu-alt-right");
+        hamburgerIcon.classList.add("bx-x");
+    } else {
+        hamburgerIcon.classList.remove("bx-x");
+        hamburgerIcon.classList.add("bx-menu-alt-right");
+    }
+};
