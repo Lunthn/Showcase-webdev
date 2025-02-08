ff7f0b11c495566436d398ee22c41e831f4c0d33
Stellar <144689207+Lunthn@users.noreply.github.com>
2025-02-08 22:28:57 +0100

    feat: some seo stuff and show active page in nav

--- UC1-Profielpagina/Showcase-Profielpagina/wwwroot/js/gdpr.js
+++ UC1-Profielpagina/Showcase-Profielpagina/wwwroot/js/gdpr.js
@@ -2,7 +2,6 @@ const gdprContainer = document.querySelector(".gdpr");
 const gdprButtons = document.querySelectorAll(".gdpr-button");
 
 document.addEventListener("DOMContentLoaded", () => {
-    //don't show gdpr if user has already accepted
     if (document.cookie.includes("accepted")) return;
     setTimeout(() => {
         gdprContainer.classList.add("show");
@@ -11,7 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
             button.addEventListener("click", () => {
                 gdprContainer.classList.remove("show");
                 if (button.id === "gdpr-accept") {
-                    // set cookie for 30 days
                     document.cookie = "gdpr=accepted; max-age=" + 60 * 60 * 24 * 30;
                 }
             });
